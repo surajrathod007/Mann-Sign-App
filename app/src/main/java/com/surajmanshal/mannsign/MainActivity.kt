6731462cb694c440d66e0c0ba55d5e14ebec5a2d
Suraj <surajsinhrathod75@gmail.com>
2022-11-25 21:49:33 +0530

    Storage read write permissions added

--- app/src/main/java/com/surajmanshal/mannsign/MainActivity.kt
+++ app/src/main/java/com/surajmanshal/mannsign/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.surajmanshal.mannsign
 
+
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBar
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.tabs.TabLayout
 import com.surajmanshal.mannsign.adapter.MainViewPagerAdapter
@@ -22,6 +28,10 @@ class MainActivity : AppCompatActivity() {
 
 
     lateinit var binding : ActivityMainBinding
+    var isRead = false
+    var isWrite = false
+
+    lateinit var permissionLauncher : ActivityResultLauncher<Array<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +39,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            isRead = it[android.Manifest.permission.READ_EXTERNAL_STORAGE] ?: isRead
+            isWrite = it[android.Manifest.permission.WRITE_EXTERNAL_STORAGE] ?: isWrite
+        }
+
+        requestPermission()
         setupViewPager()
 
     }
@@ -37,5 +53,29 @@ class MainActivity : AppCompatActivity() {
         val flist = listOf(HomeFragment(),CustomOrderFragment(),UserProfileFragment())
         binding.viewPager.adapter = MainViewPagerAdapter(flist,this@MainActivity)
         binding.bottomNavigationView.setupWithViewPager2(binding.viewPager)
+    }
+
+    fun requestPermission(){
+
+        //check permission already granted or not
+        isRead = ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        isWrite = ContextCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+        var permissionRequest : MutableList<String> = ArrayList()
+
+        if(!isRead){
+            permissionRequest.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+        if(!isWrite){
+            permissionRequest.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        if(permissionRequest.isNotEmpty()){
+            //request permission
+            permissionLauncher.launch(permissionRequest.toTypedArray())
+        }
+
+
     }
 }
--- app/src/main/java/com/surajmanshal/mannsign/ui/fragments/CustomOrderFragment.kt
+++ app/src/main/java/com/surajmanshal/mannsign/ui/fragments/CustomOrderFragment.kt
@@ -1,14 +1,18 @@
 package com.surajmanshal.mannsign.ui.fragments
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.surajmanshal.mannsign.R
+import com.surajmanshal.mannsign.databinding.FragmentCustomOrderBinding
 
 class CustomOrderFragment : Fragment() {
 
+    lateinit var binding : FragmentCustomOrderBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,16 +24,13 @@ class CustomOrderFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_custom_order, container, false)
+        binding = DataBindingUtil.bind(view)!!
 
-        setupActionBar()
-        return view
+
+        return binding.root
     }
 
-    fun setupActionBar(){
-        if(activity!= null && requireActivity().actionBar != null){
-            requireActivity().actionBar!!.title = "Custom Orders"
-        }
-    }
+
 
 
 }
