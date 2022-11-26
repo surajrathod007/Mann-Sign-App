2de7a78b0de05e392c742b29577d226ec4ced144
Suraj <surajsinhrathod75@gmail.com>
2022-11-26 16:34:00 +0530

    Home fragment design completed

--- app/src/main/java/com/surajmanshal/mannsign/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/surajmanshal/mannsign/ui/fragments/HomeFragment.kt
@@ -1,60 +1,55 @@
 package com.surajmanshal.mannsign.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.surajmanshal.mannsign.R
+import com.surajmanshal.mannsign.databinding.FragmentHomeBinding
+import com.surajmanshal.mannsign.ui.activity.CartActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+
+    lateinit var binding: FragmentHomeBinding
+    lateinit var bottomMenu: BottomSheetDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.bind(view)!!
+
+        binding.btnHamBurgur.setOnClickListener {
+            showBottomMenu()
+        }
+
+        binding.btnCart.setOnClickListener {
+            val i = Intent(requireActivity(),CartActivity::class.java)
+            startActivity(i)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun showBottomMenu() {
+        bottomMenu = BottomSheetDialog(requireContext(), R.style.BottomSheetTheme)
+        val sheetView =
+            LayoutInflater.from(requireContext()).inflate(R.layout.bottom_sheet_menu, null)
+        bottomMenu.setContentView(sheetView)
+        bottomMenu.show()
     }
+
+
 }
