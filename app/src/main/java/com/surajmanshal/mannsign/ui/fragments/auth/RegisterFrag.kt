719093c6d73d49ca1be6385f8da477ff0d62d6af
Suraj <surajsinhrathod75@gmail.com>
2022-12-15 09:48:00 +0530

    Proper live validation added in RegisterFrag.kt and material lib changed to 1.6.1

--- app/src/main/java/com/surajmanshal/mannsign/ui/fragments/auth/RegisterFrag.kt
+++ app/src/main/java/com/surajmanshal/mannsign/ui/fragments/auth/RegisterFrag.kt
@@ -7,7 +7,9 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.provider.CalendarContract
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
@@ -32,8 +34,8 @@ import kotlinx.coroutines.launch
 class RegisterFrag : Fragment() {
 
     lateinit var binding: FragRegisterBinding
-    lateinit var d : LoadingScreen
-    lateinit var dd : Dialog
+    lateinit var d: LoadingScreen
+    lateinit var dd: Dialog
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -57,20 +59,24 @@ class RegisterFrag : Fragment() {
         emailFocusListner()
         passwordFocusListner()
         phoneFocusListner()
+        textWatchers()
 
         binding.tvLoginHere.setOnClickListener {
             findNavController().navigate(R.id.action_registerFrag_to_loginFrag)
         }
         binding.btnRegister.setOnClickListener {
-            try{
+            try {
                 OneSignal.setExternalUserId(etEmail.text.toString())
-            }catch (e : Exception){
-                Functions.makeToast(requireContext(),e.message.toString())
+            } catch (e: Exception) {
+                Functions.makeToast(requireContext(), e.message.toString())
             }
 
-            Functions.makeToast(requireContext(),OneSignal.getDeviceState()?.userId.toString())
+            //Functions.makeToast(requireContext(), OneSignal.getDeviceState()?.userId.toString())
             val did = OneSignal.getDeviceState()?.userId
-            if(!isDataFillled(etEmail))else if(!isDataFillled(etPassword))else if(!isDataFillled(etMobileNo))else{
+            if (!isDataFillled(etEmail)) else if (!isDataFillled(etPassword)) else if (!isDataFillled(
+                    etMobileNo
+                )
+            ) else if (!validation()) else {
                 registerUser(User().apply {
                     emailId = etEmail.text.toString()
                     password = etPassword.text.toString()
@@ -85,7 +91,7 @@ class RegisterFrag : Fragment() {
 
     private fun emailFocusListner() {
         binding.ETEmail.setOnFocusChangeListener { _, focused ->
-            if(!focused){
+            if (!focused) {
                 binding.edEmailContainer.setHelperTextColor(ColorStateList.valueOf(Color.RED))
                 binding.edEmailContainer.helperText = validEmail()
             }
@@ -94,7 +100,7 @@ class RegisterFrag : Fragment() {
 
     private fun passwordFocusListner() {
         binding.ETPassword.setOnFocusChangeListener { _, focused ->
-            if(!focused){
+            if (!focused) {
                 binding.edPasswordContainer.setHelperTextColor(ColorStateList.valueOf(Color.RED))
                 binding.edPasswordContainer.helperText = validPassword()
             }
@@ -103,7 +109,7 @@ class RegisterFrag : Fragment() {
 
     private fun phoneFocusListner() {
         binding.etMobileNumber.setOnFocusChangeListener { _, focused ->
-            if(!focused){
+            if (!focused) {
                 binding.edPhoneContainer.setHelperTextColor(ColorStateList.valueOf(Color.RED))
                 binding.edPhoneContainer.helperText = validPhone()
             }
@@ -112,16 +118,16 @@ class RegisterFrag : Fragment() {
 
     private fun validPassword(): String? {
         val passwordText = binding.ETPassword.text.toString()
-        if(passwordText.length <8){
+        if (passwordText.length < 8) {
             return "Minimum 8 character required for password"
         }
-        if(!passwordText.matches(".*[A-Z].*".toRegex())){
+        if (!passwordText.matches(".*[A-Z].*".toRegex())) {
             return "Must contain 1 Upper-case character"
         }
-        if(!passwordText.matches(".*[a-z].*".toRegex())){
+        if (!passwordText.matches(".*[a-z].*".toRegex())) {
             return "Must contain 1 Lower-case character"
         }
-        if(!passwordText.matches(".*[@#\$%^&+=].*".toRegex())){
+        if (!passwordText.matches(".*[@#\$%^&+=].*".toRegex())) {
             return "Must contain 1 Special character (@#\$%^&+=)"
         }
         return null
@@ -129,7 +135,7 @@ class RegisterFrag : Fragment() {
 
     private fun validPhone(): String? {
         val phoneText = binding.etMobileNumber.text.toString()
-        if(phoneText.length != 10){
+        if (phoneText.length != 10) {
             return "Must be 10 digit phone number"
         }
         return null
@@ -137,18 +143,19 @@ class RegisterFrag : Fragment() {
 
     private fun validEmail(): String? {
         val emailText = binding.ETEmail.text.toString()
-        if(!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
             return "Invalid email address"
         }
         return null
     }
 
-    private fun isDataFillled(view: TextView) : Boolean{
+    private fun isDataFillled(view: TextView): Boolean {
         if (TextUtils.isEmpty(view.text.toString().trim() { it <= ' ' })) {
-            when(view){
+            when (view) {
                 binding.ETEmail -> Snackbar.make(view, "Email is required", 1000).show()
                 binding.ETPassword -> Snackbar.make(view, "Password is required", 1000).show()
-                binding.etMobileNumber -> Snackbar.make(view, "Mobile number is required", 1000).show()
+                binding.etMobileNumber -> Snackbar.make(view, "Mobile number is required", 1000)
+                    .show()
             }
             return false
         }
@@ -156,27 +163,98 @@ class RegisterFrag : Fragment() {
     }
 
 
-    private fun registerUser(user: User){
+    private fun registerUser(user: User) {
         d.toggleDialog(dd)  // show
-       lifecycleScope.launch {
-           try{
-               val register = NetworkService.networkInstance.registerUser(user)
-               onSimpleResponse("Registration",register)
-           }catch (e : Exception){
-               activity?.let { ExceptionHandler.catchOnContext(it, " ${e.message}Email already registered") }
-               d.toggleDialog(dd)
-           }
-       }
+        lifecycleScope.launch {
+            try {
+                val register = NetworkService.networkInstance.registerUser(user)
+                onSimpleResponse("Registration", register)
+            } catch (e: Exception) {
+                activity?.let {
+                    ExceptionHandler.catchOnContext(
+                        it,
+                        " ${e.message}Email already registered"
+                    )
+                }
+                d.toggleDialog(dd)
+            }
+        }
     }
-    private fun onSimpleResponse(task:String, simpleResponse: SimpleResponse){
-        if(simpleResponse.success){
+
+    private fun onSimpleResponse(task: String, simpleResponse: SimpleResponse) {
+        if (simpleResponse.success) {
             d.toggleDialog(dd)  // hide
             Toast.makeText(activity, "$task Successful", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_registerFrag_to_loginFrag)
-        }else{
+        } else {
             d.toggleDialog(dd)  // hide
             Toast.makeText(activity, simpleResponse.message, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun textWatchers() {
+        binding.ETEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.edEmailContainer.setHelperTextColor(ColorStateList.valueOf(Color.RED))
+                binding.edEmailContainer.helperText = validEmail()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+
+        binding.ETPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.edPasswordContainer.setHelperTextColor(ColorStateList.valueOf(Color.RED))
+                binding.edPasswordContainer.helperText = validPassword()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+
+        binding.etMobileNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.edPhoneContainer.setHelperTextColor(ColorStateList.valueOf(Color.RED))
+                binding.edPhoneContainer.helperText = validPhone()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+    }
+
+    fun validation(): Boolean {
+        if (!binding.edEmailContainer.helperText.isNullOrEmpty()) {
+            Snackbar.make(binding.ETEmail, "Email must be valid", 1000).show()
+            return false
+        }
+
+        if (!binding.edPhoneContainer.helperText.isNullOrEmpty()) {
+            Snackbar.make(binding.etMobileNumber, "Phone number must be valid", 1000).show()
+            return false
+        }
+
+        if (!binding.edPasswordContainer.helperText.isNullOrEmpty()) {
+            Snackbar.make(binding.ETPassword, "Passworf must be valid", 1000).show()
+            return false
+        }
+        return true
     }
 }
 
