4db2dd89f834720d6d6ad3e60425ff66739946de
Suraj <surajsinhrathod75@gmail.com>
2023-07-22 20:26:29 +0530

    Account deletion activity design done

---
+++ app/src/main/java/com/surajmanshal/mannsign/ui/activity/AccountDeleteActivity.kt
@@ -0,0 +1,12 @@
+package com.surajmanshal.mannsign.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.surajmanshal.mannsign.R
+
+class AccountDeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_delete)
+    }
+}
